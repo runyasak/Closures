c7977e412ae1a5b28c57a52a66bcb90275f22bd2
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-09 23:21:08 +0700

    feat: add calculator and operation

--- Closures/main.swift
+++ Closures/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+func calculator(n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    operation(n1, n2)
+}
+
+func add(no1: Int, no2: Int) -> Int {
+    no1 + no2
+}
+
+print(calculator(n1: 2, n2: 3, operation: add))
 
